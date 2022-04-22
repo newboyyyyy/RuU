aa841261d0c7f311f10d51f8b4465cde82c36094
Simpleyi <ww252046@gmail.com>
2022-04-22 18:23:49 +0800

    新建RuU 專案開發計畫

---
+++ src/main/java/com/simpleyi/ruu/RuUApplication.java
@@ -0,0 +1,13 @@
+package com.simpleyi.ruu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuUApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simpleyi/ruu/RuUApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simpleyi.ruu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuUApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
